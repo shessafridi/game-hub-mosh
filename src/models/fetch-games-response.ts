b279609c57869c0da4ef39c9c4d0c17a26a581b5
shessafridi <mshesskhan@gmail.com>
2023-04-18 12:31:30 +0500

    Improve game types

--- src/models/fetch-games-response.ts
+++ src/models/fetch-games-response.ts
@@ -3,6 +3,35 @@ import { Game } from './game';
 export interface FetchGamesResponse {
   count: number;
   next: string;
-  previous: string;
+  previous: null;
+  seo_title: string;
+  seo_description: string;
+  seo_keywords: string;
+  seo_h1: string;
+  noindex: boolean;
+  nofollow: boolean;
+  description: string;
+  filters: GamesFilters;
+  nofollow_collections: string[];
   results: Game[];
+}
+
+export interface GamesFilters {
+  years: FilterDefinition[];
+}
+
+export interface FilterDefinition {
+  from: number;
+  to: number;
+  filter: string;
+  decade: number;
+  years: FilterYear[];
+  nofollow: boolean;
+  count: number;
+}
+
+export interface FilterYear {
+  year: number;
+  count: number;
+  nofollow: boolean;
 }
--- src/models/game.ts
+++ src/models/game.ts
@@ -7,17 +7,112 @@ export interface Game {
   background_image: string;
   rating: number;
   rating_top: number;
-  ratings: AddedByStatus;
+  ratings: Rating[];
   ratings_count: number;
-  reviews_text_count: string;
+  reviews_text_count: number;
   added: number;
   added_by_status: AddedByStatus;
   metacritic: number;
   playtime: number;
   suggestions_count: number;
   updated: string;
-  esrb_rating: AddedByStatus;
-  platforms: any[];
+  user_game: null;
+  reviews_count: number;
+  saturated_color: string;
+  dominant_color: string;
+  platforms: PlatformElement[];
+  parent_platforms: ParentPlatform[];
+  genres: Genre[];
+  stores: Store[];
+  clip: null;
+  tags: Genre[];
+  esrb_rating: EsrbRating;
+  short_screenshots: ShortScreenshot[];
 }
 
-export interface AddedByStatus {}
+export interface AddedByStatus {
+  yet: number;
+  owned: number;
+  beaten: number;
+  toplay: number;
+  dropped: number;
+  playing: number;
+}
+
+export interface EsrbRating {
+  id: number;
+  name: string;
+  slug: string;
+}
+
+export interface Genre {
+  id: number;
+  name: string;
+  slug: string;
+  games_count: number;
+  image_background: string;
+  domain?: GameDomain;
+  language?: string;
+}
+
+export enum GameDomain {
+  AppsAppleCOM = 'apps.apple.com',
+  EpicgamesCOM = 'epicgames.com',
+  GogCOM = 'gog.com',
+  MarketplaceXboxCOM = 'marketplace.xbox.com',
+  MicrosoftCOM = 'microsoft.com',
+  NintendoCOM = 'nintendo.com',
+  PlayGoogleCOM = 'play.google.com',
+  StorePlaystationCOM = 'store.playstation.com',
+  StoreSteampoweredCOM = 'store.steampowered.com',
+}
+export interface ParentPlatform {
+  platform: EsrbRating;
+}
+
+export interface PlatformElement {
+  platform: PlatformPlatform;
+  released_at: null | string;
+  requirements_en: Requirements | null;
+  requirements_ru: Requirements | null;
+}
+
+export interface PlatformPlatform {
+  id: number;
+  name: string;
+  slug: string;
+  image: null;
+  year_end: null;
+  year_start: number | null;
+  games_count: number;
+  image_background: string;
+}
+
+export interface Requirements {
+  minimum: string;
+  recommended?: string;
+}
+
+export interface Rating {
+  id: number;
+  title: RatingTitle;
+  count: number;
+  percent: number;
+}
+
+export enum RatingTitle {
+  Exceptional = 'exceptional',
+  Meh = 'meh',
+  Recommended = 'recommended',
+  Skip = 'skip',
+}
+
+export interface ShortScreenshot {
+  id: number;
+  image: string;
+}
+
+export interface Store {
+  id: number;
+  store: Genre;
+}
