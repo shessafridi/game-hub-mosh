9dde8ac560bd4a4d2de0268a3596899fe361a305
shessafridi <mshesskhan@gmail.com>
2023-10-22 14:30:44 +0500

    Fetching games with react query

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -14,7 +14,7 @@ function GameGrid({ gameQuery }: GameGridProps) {
   const { data, error, isLoading } = useGames(gameQuery);
   const skeletons = [1, 2, 3, 4, 5, 6];
 
-  if (error) return <Text>{error}</Text>;
+  if (error) return <Text>{error?.message}</Text>;
 
   return (
     <SimpleGrid
@@ -34,7 +34,7 @@ function GameGrid({ gameQuery }: GameGridProps) {
           </GameCardContainer>
         ))}
       {!isLoading &&
-        data.map(game => (
+        data?.results.map(game => (
           <GameCardContainer key={game.id}>
             <GameCard game={game} />
           </GameCardContainer>
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,20 +1,27 @@
-import { useEffect, useState } from 'react';
+import { useQuery } from '@tanstack/react-query';
 
+import { FetchResponse } from '../models/fetch-response';
 import { Game } from '../models/game';
 import { GameQuery } from '../models/game-query';
-import useData from './useData';
+import apiClient from '../services/api-client';
 
-const useGames = (gameQuery: GameQuery) =>
-  useData<Game>(
-    '/games',
-    {
-      params: {
-        genres: gameQuery.genre?.id,
-        platforms: gameQuery.platform?.id,
-        ordering: gameQuery.sortOrder,
-        search: gameQuery.searchText,
-      },
-    },
-    [gameQuery]
-  );
+const useGames = (gameQuery: GameQuery) => {
+  return useQuery({
+    queryKey: ['games', gameQuery],
+    queryFn: ({ signal }) =>
+      apiClient
+        .get<FetchResponse<Game>>('/games', {
+          params: {
+            genres: gameQuery.genre?.id,
+            parent_platforms: gameQuery.platform?.id,
+            ordering: gameQuery.sortOrder,
+            search: gameQuery.searchText,
+          },
+          signal,
+        })
+        .then(res => res.data),
+    staleTime: 60 * 1000,
+  });
+};
+
 export default useGames;
