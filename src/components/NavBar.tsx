ffa787a24989e1f58984fbe83bf54e7743c7169d
shessafridi <mshesskhan@gmail.com>
2023-10-23 16:27:11 +0500

    Fix the navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,3 +1,5 @@
+import { Link, useNavigate } from 'react-router-dom';
+
 import { HStack, Image } from '@chakra-ui/react';
 
 import logo from '../assets/logo.webp';
@@ -6,12 +8,20 @@ import ColorModeSwitch from './ColorModeSwitch';
 import SearchInput from './SearchInput';
 
 function NavBar() {
+  const navigate = useNavigate();
   const setSearchText = useGameQueryStore(s => s.setSearchText);
 
   return (
     <HStack padding='10px'>
-      <Image boxSize='60px' src={logo}></Image>
-      <SearchInput onSearch={setSearchText} />
+      <Link style={{ display: 'contents' }} to='/'>
+        <Image boxSize='60px' src={logo}></Image>
+      </Link>
+      <SearchInput
+        onSearch={searchText => {
+          setSearchText(searchText);
+          navigate('/');
+        }}
+      />
       <ColorModeSwitch />
     </HStack>
   );
